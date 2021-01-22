541b420040b181dedc979f37a790bc86211fcdb1
Luan Thome <38923629+luanripax@users.noreply.github.com>
2021-01-22 15:36:58 -0300

    Update ParseDoc.java
    removed useless

--- ParseDoc.java
+++ ParseDoc.java
@@ -22,32 +22,9 @@ public class Parse {
          NodeList nList = doc.getElementsByTagName("POS-DOUTORADO");
          System.out.println("---Pos-Doutorado---");
          if(nList.getLength() == 0) {
-        	 System.out.println("nao possui pos doutorado");
+        	 System.out.println("Não possui pós-doutorado");
          }
-         
-         /*
-         for (int i = 0; i < nList.getLength(); i++) {
-             Node No = nList .item(i);
-             // System.out.println("\nCurrent Element :" + nNode.getNodeName());
-             
-             if (No.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) No;
-                //eElement.getElementsByTagName("DADOS-BASICOS-DO-TRABALHO");
-                if(eElement.getAttribute("NOME-INSTITUICAO") != null) {
-                	System.out.println("nao possui pos doutorado");
-                }
-                System.out.println("Titulo : " 
-                   + eElement.getAttribute("TITULO-DO-TRABALHO-DE-CONCLUSAO-DE-CURSO"));
-                System.out.println("Instituicao : " 
-                        + eElement.getAttribute("NOME-INSTITUICAO"));
-                System.out.println("Ano de conclus�o : " 
-                                + eElement.getAttribute("ANO-DE-CONCLUSAO"));
-             }
-          }
-         	*/
-
-         
-         
+           
       } catch (Exception e) {
          e.printStackTrace();
       }
